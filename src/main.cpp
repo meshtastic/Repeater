e248e6960d5c6ee91e2334e72bc1897f31de8388
Thomas Göttgens <tgoettgens@gmail.com>
2022-04-05 16:27:45 +0200

    Move to 1.3 codebase, not working yet

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,19 @@
 #include <Base64.h>
 #include <LoRa_APP.h>
 #include "cyPm.c"
+#include <pb_decode.h>
 
 // CONFIGURATION:
 // Change RegionCode config.h !
 #define VERBOSE         // define to SILENT to turn off serial messages
-#define NOBLINK        // define to NOBLINK to turn off LED signaling
-#define NO_OLED        // define to NO_OLED to turn off display
+// #define NOBLINK        // define to NOBLINK to turn off LED signaling
+// #define NO_OLED        // define to NO_OLED to turn off display
                        // OLED supported for cubecell_board_Plus (HTCC-AB02) and cubecell_gps (HTCC-AB02S)
 // :CONFIGURATION
 
 static MeshPacket       thePacket;
-static ChannelSettings  ChanSet;
+static ChannelSet       ChanSet;
+static ChannelSettings  CSet;
 static RadioEvents_t    RadioEvents;
 static TimerEvent_t     CheckRadio;
 static uint32_t         lastreceivedID = 0;
@@ -28,15 +30,22 @@ CubeCell_NeoPixel LED(1, RGB, NEO_GRB + NEO_KHZ800);
 
 #ifndef NO_OLED
     #ifdef CubeCell_BoardPlus
-        #include "cubecell_SH1107Wire.h"
-        SH1107Wire  display(0x3c, 500000, I2C_NUM_0,GEOMETRY_128_64,GPIO10 );
+        #include "HT_SH1107Wire.h"
+        SH1107Wire  display(0x3c, 500000, SDA, SCL, GEOMETRY_128_64, GPIO10); 
     #endif
     #ifdef CubeCell_GPS
-        #include "cubecell_SSD1306Wire.h"
-        SSD1306Wire  display(0x3c, 500000, I2C_NUM_0,GEOMETRY_128_64,GPIO10 );
+        #include "HT_SSD1306Wire.h"
+        SSD1306Wire display(0x3c, 500000, SDA, SCL, GEOMETRY_128_64, GPIO10); 
     #endif
 char str[32];
 #endif
+
+bool ChannelSet_callback(pb_istream_t *istream, const pb_field_iter_t *field, void **arg)
+{
+    ChannelSettings * dest = (ChannelSettings*)(*arg);
+    ChannelSettings target = *dest;
+    return true;
+}
 
 void setup() {
     TimerInit( &CheckRadio, onCheckRadio );
@@ -76,54 +85,79 @@ void setup() {
     // import Channel Settings from Meshtastic Link (channel name, modem config):
     char decoded[Base64.decodedLength(&MeshtasticLink[30], sizeof(MeshtasticLink)-30)];
     Base64.decode(decoded, &MeshtasticLink[30], sizeof(MeshtasticLink)-30);
-    pb_decode_from_bytes((uint8_t *)&decoded[0], sizeof(decoded), ChannelSettings_fields, &ChanSet);
+    ChanSet.settings.arg = &CSet;
+    ChanSet.settings.funcs.decode = ChannelSet_callback;
+    pb_decode_from_bytes((uint8_t *)&decoded[0], sizeof(decoded), ChannelSet_fields, &ChanSet);
     // done, populate remaining settings according to channel name and provided modem config
-    ChanSet.channel_num = hash( ChanSet.name ) % regions[REGION].numChannels; // see config.h
-    ChanSet.tx_power    = (regions[REGION].powerLimit == 0) ? TX_MAX_POWER : MIN(regions[REGION].powerLimit, TX_MAX_POWER) ;
-    /* FYI:
+        CSet.channel_num = 6; // hash( CSet.name ) % regions[REGION].numChannels; // see config.h
+    // TODO: this is hardcoded for now cause the protobuf decode doesn't seem to work right.
+    CSet.tx_power    = (regions[REGION].powerLimit == 0) ? TX_MAX_POWER : MIN(regions[REGION].powerLimit, TX_MAX_POWER) ;
+    /* FYI: 
     "bandwidth":
     [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: 62.5kHz, 4: 41.67kHz, 5: 31.25kHz, 6: 20.83kHz, 7: 15.63kHz, 8: 10.42kHz, 9: 7.81kHz]
     "speed":
-        0: ChannelSettings_ModemConfig_Bw125Cr45Sf128 aka short range
-        1: ChannelSettings_ModemConfig_Bw500Cr45Sf128 aka medium range
-        2: ChannelSettings_ModemConfig_Bw31_25Cr48Sf512 aka long range
-        3: ChannelSettings_ModemConfig_Bw125Cr48Sf4096  aka very long range
+        0: ChannelSettings_ModemConfig_VLongSlow SF12 
+        1: ChannelSettings_ModemConfig_LongSlow SF12
+        2: ChannelSettings_ModemConfig_LongFast SF11
+        3: ChannelSettings_ModemConfig_MidSlow SF10
+        4: ChannelSettings_ModemConfig_MidFast SF9
+        5: ChannelSettings_ModemConfig_ShortSlow SF8
+        6: ChannelSettings_ModemConfig_ShortFast SF7
+
     "coding rate":
         [1: 4/5, 2: 4/6, 3: 4/7, 4: 4/8]
     */
-    switch ( (uint8_t)ChanSet.modem_config ){
-        case 0: {  // short range
-            ChanSet.bandwidth = 0;      // 125 kHz
-            ChanSet.coding_rate = 1;    // = 4/5
-            ChanSet.spread_factor = 7;
+    switch ( (uint8_t)CSet.modem_config ){
+        case 0: {  // VLongSlow
+            CSet.bandwidth = 5; // 31.25
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 12;
             break;
         }
-        case 1: {  // medium range
-            ChanSet.bandwidth = 2;      // 500 kHz
-            ChanSet.coding_rate = 1;    // = 4/5
-            ChanSet.spread_factor = 7;
+        case 1: {  // LongSlow
+            CSet.bandwidth = 0; // 125
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 12;
             break;
         }
-        case 2: {  // long range
-            ChanSet.bandwidth = 5;      // 31.25 kHz
-            ChanSet.coding_rate = 4;    // = 4/8
-            ChanSet.spread_factor = 9;
+        case 2: {  // LongFast
+            CSet.bandwidth = 1; // 250
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 11;
             break;
         }
-        case 3: {  // very long range
-            ChanSet.bandwidth = 0;      // 125 kHz
-            ChanSet.coding_rate = 4;    // = 4/8
-            ChanSet.spread_factor = 12;
+        case 3: {  // MidSlow 
+            CSet.bandwidth = 1; // 250
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 10;
             break;
         }
-        default:{  // default setting is very long range
-            ChanSet.bandwidth = 0;      // 125 kHz
-            ChanSet.coding_rate = 4;    // = 4/8
-            ChanSet.spread_factor = 12;
+        case 4: {  // MidFast
+            CSet.bandwidth = 1; // 250
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 9;
+            break;
+        }
+        case 5: {  // ShortSlow 
+            CSet.bandwidth = 1; // 250
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 8;
+            break;
+        }
+        case 6: {  // ShortFast 
+            CSet.bandwidth = 1; // 250
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 7;
+            break;
+        }
+        default:{  // default setting is LongFast
+            CSet.bandwidth = 1; // 250
+            CSet.coding_rate = 4; // 4/8
+            CSet.spread_factor = 11;
         }
     }
-    symbolTime =  ((1<< ChanSet.spread_factor)*1000 +32) *1000 / TheBandwidths[ChanSet.bandwidth]; // in micro seconds!
-    ConfigureRadio( ChanSet );
+    symbolTime =  getPacketTime(16); // in micro seconds!
+    ConfigureRadio( CSet );
 #ifndef SILENT
     MSG("\n..done!\n");
     LINE(60,"*");
@@ -135,6 +169,27 @@ void setup() {
     TimerSetValue( &CheckRadio, symbolTime / 100 ); // MCU sleeps 10 LoRa symbols (in milli seconds)
     TimerStart( &CheckRadio );                      // onCheckRadio() will break deep sleep mode
     Radio.StartCad( 3 ); // length in symbols
+}
+
+uint32_t getPacketTime(uint32_t pl)
+{
+    float bandwidthHz = TheBandwidths[CSet.bandwidth] * 1000.0f;
+    bool headDisable = false; // we currently always use the header
+    float tSym = (1 << CSet.spread_factor) / bandwidthHz;
+
+    bool lowDataOptEn = tSym > 16e-3 ? true : false; // Needed if symbol time is >16ms
+
+    float tPreamble = (LORA_PREAMBLE_LENGTH + 4.25f) * tSym;
+    float numPayloadSym =
+        8 + max(ceilf(((8.0f * pl - 4 * CSet.spread_factor + 28 + 16 - 20 * headDisable) / (4 * (CSet.spread_factor - 2 * lowDataOptEn))) * CSet.coding_rate), 0.0f);
+    float tPayload = numPayloadSym * tSym;
+    float tPacket = tPreamble + tPayload;
+
+    uint32_t msecs = tPacket * 1000;
+
+    MSG("(bw=%d, sf=%d, cr=4/%d) packet symLen=%d ms, payloadSize=%u, time %d ms\n", (int)TheBandwidths[CSet.bandwidth], CSet.spread_factor, CSet.coding_rate, (int)(tSym * 1000),
+              pl, msecs);
+    return msecs;
 }
 
 void onCheckRadio(void)
@@ -224,9 +279,9 @@ void onRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
     p->id   = h->id;
     p->hop_limit = h->flags && 0b00000111;
     p->want_ack  = h->flags && 0b00001000;
-    p->which_payload = MeshPacket_encrypted_tag;
+    p->which_payloadVariant = MeshPacket_encrypted_tag;
     p->encrypted.size= size - sizeof(PacketHeader);
-    p->decoded.data.payload.size = p->encrypted.size;
+    p->decoded.payload.size = p->encrypted.size;
     memcpy(p->encrypted.bytes, payload + sizeof(PacketHeader), p->encrypted.size);
 #ifndef SILENT
     MSG("\nReceived packet! (Size %i bytes, RSSI %i, SNR %i)\n", size, rssi, snr);
@@ -255,7 +310,7 @@ void onRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
     display.drawString(40,32,str);
     display.display();
 #endif
-    if ( !(lastreceivedID == thePacket.id) ){
+    if ( !(lastreceivedID == thePacket.id) ){ 
         // will repeat package
         lastreceivedID = thePacket.id;
         #ifndef NO_OLED
@@ -270,7 +325,7 @@ void onRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
         #ifndef SILENT
             MSG("Sending packet..");
             sendTime = millis();
-        #endif
+        #endif 
         Radio.Send( payload, size );
      }
     else{
@@ -282,12 +337,12 @@ void onRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr )
             display.drawString(0,53,str);
             display.display();
         #endif
-    }
+    } 
 }
 
 // hash a string into an integer - djb2 by Dan Bernstein. -
 // http://www.cse.yorku.ca/~oz/hash.html
-unsigned long hash(char *str)
+unsigned long hash(char *str) 
 {
     unsigned long hash = 5381;
     int c;
@@ -298,7 +353,9 @@ unsigned long hash(char *str)
 
 void ConfigureRadio( ChannelSettings ChanSet )
 {
-    uint32_t freq = (regions[REGION].freq + regions[REGION].spacing * ChanSet.channel_num)*1E6;
+    // uint32_t freq = (regions[REGION].freq + regions[REGION].spacing * ChanSet.channel_num)*1E6;
+    uint32_t numChannels = floor((regions[REGION].freqEnd - regions[REGION].freqStart) / (regions[REGION].spacing + (TheBandwidths[CSet.bandwidth] / 1000)));
+    uint32_t freq = regions[REGION].freqStart + ((((regions[REGION].freqEnd - regions[REGION].freqStart) / numChannels) / 2) * ChanSet.channel_num);
     #ifndef SILENT
     MSG("\nRegion is: %s", regions[REGION].name);
     MSG("  TX power: %i\n", ChanSet.tx_power);
@@ -322,7 +379,7 @@ void MCU_deepsleep(void)
     UART_1_Sleep;
     #endif
     pinMode(P4_1, ANALOG); // SPI0 MISO
-    CySysPmDeepSleep(); // deep sleep mode
+    CySysPmDeepSleep(); // deep sleep mode    
     systime = (uint32_t)TimerGetCurrentTime();
     pinMode(P4_1, INPUT);
     #ifndef SILENT
